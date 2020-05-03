4a51b1b5ee82de484702a47794633d74cf4d1a28
YN <yo.z940127@gmail.com>
2020-05-03 20:18:58 +0900

    Modify setting

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -8,13 +8,6 @@ module.exports = merge(webpackConfig, {
     inline: true,
     open: true,
     host: 'localhost',
-    port: 8080,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
-        logLevel: 'debug'
-      }
-    }
+    port: 8080
   }
 })
